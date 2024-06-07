9eae3e3446f333ccc4ac04b910af85ed98406cf3
Rithik Singh <69286842+Rithik-dotcom@users.noreply.github.com>
2024-06-07 22:59:56 +0530

    Main Branch

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export const settings = {
+    app: {
+        name: "Geeks NextJs",
+        version: "2.2.1"
+    },
+    theme: {
+        skin: "light" // possible values are light, dark
+    }
+};
+export default { settings };
